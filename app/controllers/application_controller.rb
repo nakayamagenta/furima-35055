39df2a10f6237ecf7a6f38057d3d46dfded86907
nakayamagenta <80379071+nakayamagenta@users.noreply.github.com>
2021-04-08 12:31:36 +0900

    新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :encrypted_password, :nickname, :birthday])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
+  end
+
   has_many :items
   has_many :purchases
 end
