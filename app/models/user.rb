268e63cf90440fca9478b18f4d07319ac0119431
nakayamagenta <80379071+nakayamagenta@users.noreply.github.com>
2021-04-10 10:56:07 +0900

    ユーザー登録機能　修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,16 @@ class User < ApplicationRecord
     validates :birthday
   end
 
-  with_options format: {with: /\A[ァ-ヶー]+\z/} do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-
-  with_options format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+  with_options format:{with: /\A[ぁ-んァ-ン一-龥]/} do
    validates :first_name 
    validates :last_name
    validates :first_name_kana
    validates :last_name_kana
+  end
+
+  with_options format: {with: /\A[ァ-ヶー]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
   end
 
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     email                 {Faker::Internet.free_email}
     password              { '1a' + Faker::Internet.password(min_length: 4) }
-    encrypted_password    {password}
+    password_confirmation {password}
     last_name             {"山だ"}
     last_name_kana          {"ヤマダ"}
     first_name            {"太ろう"}
