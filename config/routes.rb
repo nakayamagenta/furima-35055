e936657f85b0b3416d46cfa04a22ed74e75d82f1
nakayamagenta <80379071+nakayamagenta@users.noreply.github.com>
2021-04-15 18:49:45 +0900

    商品一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index' 
   resources :articles
-  resources :items, only: [:new, :create]
+  resources :items, only: [:new, :create, :index]
 end
