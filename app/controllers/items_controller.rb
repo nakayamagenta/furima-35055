d7deb28db1cd8192e83d2d16fb51ed7d8371294a
nakayamagenta <80379071+nakayamagenta@users.noreply.github.com>
2021-04-16 18:07:46 +0900

    商品編集　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  #before_action :set_item, only: [:edit, :show]
-
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_items, only: [:edit, :update]
+  
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -20,19 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   #def destroy
   #end
 
   def update
-     @item= Item.find(params[:id])
     if @item.update(item_params)
     redirect_to item_path
     else
@@ -46,5 +44,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product, :category_id, :price, :condiction_id, :shipping_chager_id, :shipping_area_id, :shipping_day_id, :explanation, :image).merge(user_id: current_user.id)
                              
+   def set_item
+    @item= Item.find(params[:id])
+   end
+
+   def set_items
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
   end
 end
