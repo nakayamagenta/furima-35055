8261acafea2690f09fe973117f99d0a97690f8c4
nakayamagenta <80379071+nakayamagenta@users.noreply.github.com>
2021-04-16 18:15:33 +0900

    商品編集　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :set_items, only: [:edit, :update]
-  
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -21,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def show
